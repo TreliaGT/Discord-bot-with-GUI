eb6c50ac533283c5d15b0044cf8d883c9c09552f
SpaceOtter <42791605+TreliaGT@users.noreply.github.com>
2021-03-05 11:15:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,45 @@
 # bot.py
 import sys
 import discord 
-import asyncio
 from dotenv import load_dotenv
 import os
-from gui import app 
-import tkinter as tk
-import _thread
 import threading
-load_dotenv()
-client = discord.Client()
-botchatid = int(os.getenv('BOTCHAT'))
-token = os.getenv('TOKEN')
-checker = threading.Event()
-loop = asyncio.new_event_loop()
+
+class botclass(discord.Client):
+	load_dotenv()
+	botchatid = int(os.getenv('BOTCHAT'))
+	token = os.getenv('TOKEN')
+
+	async def on_ready(self):
+		print('We have logged in as {0.user}'.format(self))
+
+	async def on_message(self , message):
+		if message.author == self.user:
+			return
+			
+		if message.content.startswith('$text'):
+			channel = self.get_channel(self.botchatid)
+			await channel.send('Hello There')
+			
+		if message.content.startswith('$info'):
+			channel = self.get_channel(self.botchatid)
+			await channel.send("Hello")
+		
+	async def message(self , message):
+		channel = self.get_channel(self.botchatid)
+		await self.send(message)
+		
+
+class loadbot(threading.Thread): 
+	def __init__(self):
+		threading.Thread.__init__(self)
+		
+	def run():
+		load_dotenv()
+		token = os.getenv('TOKEN')
+		botclass.run(token)
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-	
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-        
-    if message.content.startswith('$text'):
-        channel = client.get_channel(botchatid)
-        await channel.send('Hello There')
-        
-    if message.content.startswith('$info'):
-        channel = client.get_channel(botchatid)
-        await channel.send("Hello")
-	
-    await client.process_commands(message)
-    
-def lup():
-    loop.run_until_complete(client.start(token))
-
-
-#threading.Thread(target=lup,args=(client.run(token),)).start()
-#threading.Thread(target=app).start()
-
-app();
 
 
 
